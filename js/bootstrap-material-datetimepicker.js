65b94d80c08a9c150c092aa1be2bce0cd005f34d
Eduard Panfile <epanfile@gmail.com>
2023-02-16 12:50:50 +0200

    Update bootstrap-material-datetimepicker.js

--- js/bootstrap-material-datetimepicker.js
+++ js/bootstrap-material-datetimepicker.js
@@ -1,5 +1,6 @@
 (function ($, moment)
 {
+    var $ = window.$;
     var pluginName = "bootstrapMaterialDatePicker";
     var pluginDataName = "plugin_" + pluginName;
     var mdiClose = "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z";
@@ -39,7 +40,7 @@
         this.init();
     }
 
-    window.$.fn[pluginName] = function (options, p)
+    $.fn[pluginName] = function (options, p)
     {
         this.each(function ()
         {
